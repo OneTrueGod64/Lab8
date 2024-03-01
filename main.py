fa53d47907e5e8680efaa08f2a3c8dc55f8b94e5
Tyler-907 <130415751+Tyler-907@users.noreply.github.com>
2024-03-01 15:30:25 -0800

    main.py

--- main.py
+++ main.py
@@ -2,35 +2,29 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 from AboutApp import AboutApp
-from GramsToOuncesApp import GramsToOuncesApp
-
-# Main Window for the team calculator project.
-# To add a new tab, create an App object with a get_top_frame method.
-# Then, add the app to the __main_notebook, along with a name for the tab.
-# See GramsToOuncesApp.py for a simple example.
+# Import the CelsiusToFahrenheitApp class
+from CelsiusToFahrenheitApp import CelsiusToFahrenheitApp
 
 class MainApp:
     def __init__(self, master):
-        # This is needed to allow the notebook tabs to stretch.
         tk.Grid.columnconfigure(master, 0, weight=1)
         tk.Grid.rowconfigure(master, 0, weight=1)
 
-        # build ui
         self.__main_notebook = ttk.Notebook(master)
         self.__main_notebook.grid(column='0', row='0', sticky='nsew')
         self.__main_notebook.rowconfigure('0', weight='1')
         self.__main_notebook.columnconfigure('0', weight='1')
 
-        # Main widget
         self.__mainwindow = self.__main_notebook
 
         # Add About... tab
         about_app = AboutApp(self.__mainwindow)
         self.__main_notebook.add(about_app.get_top_frame(), text="About...")
 
-        # Add first calculator
-        grams_to_ounces_app = GramsToOuncesApp(self.__mainwindow)
-        self.__main_notebook.add(grams_to_ounces_app.get_top_frame(), text="Grams to Ounces")
+        # Add Celsius to Fahrenheit calculator
+        # Assuming the CelsiusToFahrenheitApp class exists and follows a similar interface
+        celsius_to_fahrenheit_app = CelsiusToFahrenheitApp(self.__mainwindow)
+        self.__main_notebook.add(celsius_to_fahrenheit_app.get_top_frame(), text="Celsius to Fahrenheit")
 
     def run(self):
         self.__mainwindow.mainloop()
@@ -39,4 +33,3 @@ if __name__ == '__main__':
     root = tk.Tk()
     app = MainApp(root)
     app.run()
-
